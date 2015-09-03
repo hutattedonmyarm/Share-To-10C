7690f6d07cedd22792217f894a1f030e4d333293
Max Nuding <hutattedonmyarm@icloud.com>
2015-09-03 20:33:32 +0200

    Tried some stuff with networking. failed

--- Post To 10C/ShareViewController.m
+++ Post To 10C/ShareViewController.m
@@ -8,7 +8,7 @@
 
 #import "ShareViewController.h"
 
-@interface ShareViewController ()
+@interface ShareViewController () <NSURLSessionDelegate, NSURLSessionDataDelegate, NSURLSessionTaskDelegate>
 @property (unsafe_unretained) IBOutlet NSTextView *textView;
 @property (weak) IBOutlet NSTextField *remainingCharactersLabel;
 @property (weak) IBOutlet NSButton *postButton;
@@ -23,6 +23,11 @@
 - (NSString *)nibName {
     return @"ShareViewController";
 }
+
+/***************************************************************************************
+ * Warning: Big mess currently due to various attempts at playing with networking code *
+ ***************************************************************************************/
+
 - (void)loadView {
     //Auth: https://account.app.net/oauth/authenticate?client_id=LzpEruz978ZHrpdRueMeMzDmUd4hEuyK&response_type=token&scope=messages:net.app.core.pm
     [super loadView];
@@ -48,6 +53,8 @@
         self.authorizedLabel.stringValue = @"Authorized";
         self.authorizedLabel.textColor = [NSColor greenColor];
     }
+    NSURL *channelURL = [NSURL URLWithString:@"https://api.app.net/channels"];
+    [self makeAPIRequestWithURL:channelURL andMethod:@"GET" andHeader:nil andData:nil];
 }
 
 -(void)textDidChange:(NSNotification *)notification {
@@ -60,9 +67,26 @@
         self.postButton.enabled = YES;
     }
 }
-
+-(void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveData:(NSData *)data {
+    NSLog(@"data!");
+    
+}
+-(void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveResponse:(NSURLResponse *)response completionHandler:(void (^)(NSURLSessionResponseDisposition))completionHandler {
+    NSLog(@"response");
+}
+-(void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error {
+    NSLog(@"shit: %@", error);
+}
 - (IBAction)send:(id)sender {
     
+    //NSDictionary *json = [self makeAPIRequestWithURL:channelURL andMethod:@"GET" andHeader:nil andData:nil];
+    //for (NSDictionary *dict in json[@"data"]) {
+        //bool newMessage = [((NSNumber *)dict[@"has_unread"]) boolValue] && [@"net.app.core.pm" isEqualToString:dict[@"type"]];
+        //if (newMessage) {
+        //    [self performSelectorInBackground:@selector(displayNotificationWithIDs:) withObject:@[dict[@"id"], dict[@"recent_message_id"]]];
+        //}
+        //}
+        //NSLog(@"%@", json);
     // Complete implementation by setting the appropriate value on the output item
     if (self.textView.string.length > kADN_PM_LIMIT) {
         NSLog(@"Too long");
@@ -77,9 +101,62 @@
     
 }
 
+- (NSURLSession *) configureMySession {
+    NSURLSession *mySession = nil;
+    NSURLSessionConfiguration* config = [NSURLSessionConfiguration backgroundSessionConfigurationWithIdentifier:@"com.hutattedonmyarm.posttotenc.getchannelsession"];
+    // To access the shared container you set up, use the sharedContainerIdentifier property on your configuration object.
+    config.sharedContainerIdentifier = @"group.hutattedonmyarm.posttotenc.app";
+    mySession = [NSURLSession sessionWithConfiguration:config delegate:self delegateQueue:nil];
+    return mySession;
+}
+
+-(void)URLSession:(NSURLSession *)session didBecomeInvalidWithError:(NSError *)error {
+    NSLog(@"invalid: %@", error);
+}
+
 - (IBAction)cancel:(id)sender {
     NSError *cancelError = [NSError errorWithDomain:NSCocoaErrorDomain code:NSUserCancelledError userInfo:nil];
     [self.extensionContext cancelRequestWithError:cancelError];
+}
+
+-(NSDictionary *)makeAPIRequestWithURL:(NSURL *)requestURL andMethod:(NSString *)method andHeader:(NSDictionary *)header andData:(NSData *)data{
+    NSDictionary *json = nil;
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:requestURL];
+    NSURLResponse *response = nil;
+    NSError *error = nil;
+    request.HTTPMethod = method;
+    NSUserDefaults *mySharedDefaults = [[NSUserDefaults alloc] initWithSuiteName: @"group.hutattedonmyarm.posttotenc.app"];
+    [request setValue:[NSString stringWithFormat:@"Bearer %@",
+                       [mySharedDefaults stringForKey:@"authToken"]] forHTTPHeaderField:@"Authorization"];
+    for(NSString *key in [header allKeys]) {
+        [request setValue:header[key] forHTTPHeaderField:key];
+    }
+    if (data != nil) {
+        request.HTTPBody = data;
+    }
+    NSURLSession *mySession = [self configureMySession];
+    NSURLSessionTask *myTask = [mySession dataTaskWithURL:requestURL];
+    [myTask resume];
+
+    /*NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    if(error != nil) {
+        NSLog(@"ADN Error:%@", error);
+        
+        //We need to dog deeper here than just assume it's an authentication
+        NSLog(@"Invalid auth token!");
+        return nil;
+    } else {
+        if (responseData != nil) {
+            NSError *jError = nil;
+            json = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&jError];
+        }
+    }
+    //Checking if our auth token is valid
+    if (json != nil && ([json[@"meta"][@"code"] integerValue] == 401 || [json[@"meta"][@"code"] integerValue] == 403)) {
+        NSLog(@"Invalid auth token!");
+        return nil;
+    }*/
+    return json;
 }
 
 @end
